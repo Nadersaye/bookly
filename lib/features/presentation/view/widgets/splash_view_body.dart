f5f86aa7d6792d067a90525f393bc7ec35bccffc
Nader <nsyd1582@gmail.com>
2023-08-01 23:36:47 +0300

    add animation to the text of splah

--- lib/features/presentation/view/widgets/splash_view_body.dart
+++ lib/features/presentation/view/widgets/splash_view_body.dart
@@ -1,20 +1,52 @@
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/presentation/view/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(ImagePath.logo),
-        const Text('Read Free Books',
-        textAlign: TextAlign.center,
+        const SizedBox(
+          height: 15,
         ),
+        SlidingAnimationText(slidingAnimation: slidingAnimation)
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 4));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero).animate(
+            CurvedAnimation(
+                parent: animationController, curve: Curves.elasticIn));
+    animationController.forward();
   }
 }
