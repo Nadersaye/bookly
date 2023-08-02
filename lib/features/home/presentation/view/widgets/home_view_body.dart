049ec778e7cf25816edd7c359fe2746ae9e83894
Nader <nsyd1582@gmail.com>
2023-08-02 15:16:30 +0300

    create horizental list view item of home view

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
+import 'h_list_view_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({
@@ -9,7 +10,10 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [CustomAppBar()],
+      children: [
+        CustomAppBar(),
+        HomeHListViewItem(),
+      ],
     );
   }
 }
