99680283e5479dac55ae786b1d1bc621710dca1c
Nader <nsyd1582@gmail.com>
2023-08-22 05:56:31 +0300

    create newset books states

--- lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -1,4 +1,5 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 import '../../../data/models/book_model/book_model.dart';
@@ -6,5 +7,15 @@ import '../../../data/models/book_model/book_model.dart';
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+  Future fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var responce = await homeRepo.fetchNewsetBooks();
+    responce.fold((failure) {
+      emit(NewestBooksFailure(failure.errorMessage));
+    }, (books) {
+      emit(NewestBooksSuccess(books));
+    });
+  }
 }
