ce278d2bcf78c56fb51d43df38f6e72d8490e683
Nader <nsyd1582@gmail.com>
2023-08-26 21:11:14 +0300

    implement fetch featured books

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -6,4 +6,6 @@ import '../../../../core/errors/failures.dart';
 abstract class HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchNewsetBooks();
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
+      {required String category});
 }
